5f4f63e080d1aba01fa49555bffadbc595599c30
Daniel Marais 219476845 <>
2021-04-01 21:54:58 +0200

    Final Commit of Daniel 219476845

--- src/main/java/com/mycompany/groupadp2/Employer.java
+++ src/main/java/com/mycompany/groupadp2/Employer.java
@@ -10,5 +10,32 @@ package com.mycompany.groupadp2;
  * @author Daniel 219476845
  */
 public class Employer {
+
+      private String name;
+    private int a,b;
     
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name= name;
+    }
+        public int getA(){
+            return a;
+        }
+        public void setA(int a){
+            this.a=a;
+        }
+        public int getB(){
+            return b;
+        }
+        public void setB(int b){
+            this.b=b;
+        }
+
+    @Override
+    public String toString() {
+        return "Gamer{" + "name=" + name + ", a=" + a + ", b=" + b + '}';
+    }
+
 }
